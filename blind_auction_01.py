eeddff34d6bb8e2a9659d607461fb793b744424a
Nesta Parchment <saint.clever.np@gmail.com>
2020-11-23 23:43:59 -0500

    update def auction max_bid var

--- blind_auction_01.py
+++ blind_auction_01.py
@@ -4,24 +4,24 @@ import os
 print(logo)
 name_and_bid = {}
 
-''' clear method '''
+''' Clear terminal '''
 def clear():
-	os.system('cls' if os.name == 'nt' else 'clear')
+  os.system('cls' if os.name == 'nt' else 'clear')
 
-''' auction method '''
+''' Get bidder name and bid '''
 def auction():
-	username = input('Name: ')
-	user_bid = int(input('Bid: '))
-	name_and_bid.update({username: user_bid})
+  username = input('Name: ').capitalize()
+  user_bid = int(input('Bid: '))
+  name_and_bid.update({username: user_bid})
+  other_bidders = input('Are there other users who want to bid: Y/N ').lower()
+  clear()
 
-	other_bidders = input('Are there other users who want to bid: Y/N ').lower()
-	clear()
-	
-	if other_bidders == 'n':
-		# print(name_and_bid)	
-		print(f'{max(name_and_bid, key=name_and_bid.get)} has the highest bid: ${name_and_bid[max(name_and_bid, key=name_and_bid.get)]}')
-		return name_and_bid
-	elif other_bidders == 'y':
-		auction() # recursive function
+  max_bid = max(name_and_bid, key=name_and_bid.get)
+  if other_bidders == 'n':
+    # print(name_and_bid)
+    print(f'{max_bid} has the highest bid of ${name_and_bid[max_bid]}')
+    return name_and_bid
+  elif other_bidders == 'y':
+    auction() # recursive function
 
 auction()
